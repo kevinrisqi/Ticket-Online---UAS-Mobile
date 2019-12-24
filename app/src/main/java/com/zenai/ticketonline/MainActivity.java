8dedd679fa1178b1f61495e1bc583c58a5251ce2
Kevin <kevinrisqi22@gmail.com>
2019-12-24 18:47:10 +0700

    Add List Data and View Design

--- app/src/main/java/com/zenai/ticketonline/MainActivity.java
+++ app/src/main/java/com/zenai/ticketonline/MainActivity.java
@@ -19,6 +19,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.zenai.ticketonline.models.data_mahasiswa;
 
 import java.util.Collections;
 
---
+++ app/src/main/java/com/zenai/ticketonline/MyListData.java
@@ -0,0 +1,14 @@
+package com.zenai.ticketonline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyListData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_list_data);
+    }
+}
