e69e373a3157e01fe96ffff3f30e2e74d4cfd055
Kevin Risqi <kevinrisqi22@gmail.com>
2019-12-26 20:33:13 +0700

    Add data_wisata models

--- app/src/main/java/com/zenai/ticketonline/models/data_wisata.java
+++ app/src/main/java/com/zenai/ticketonline/models/data_wisata.java
@@ -1,4 +1,51 @@
 package com.zenai.ticketonline.models;
 
 public class data_wisata {
+    private String nama;
+    private String lokasi;
+    private String deskripsi;
+    private int harga;
+
+    public data_wisata(){
+
+    }
+
+    public data_wisata(String nama, String lokasi, String deskripsi, int harga) {
+        this.nama = nama;
+        this.lokasi = lokasi;
+        this.deskripsi = deskripsi;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
 }
