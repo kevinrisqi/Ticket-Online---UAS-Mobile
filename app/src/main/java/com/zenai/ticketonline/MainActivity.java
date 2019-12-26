e2c7d4b6cd0496ebf1965de0effd040aa319fe6f
Kevin Risqi <kevinrisqi22@gmail.com>
2019-12-26 21:39:49 +0700

    Wisata Fragments Fails

--- app/src/main/java/com/zenai/ticketonline/MainActivity.java
+++ app/src/main/java/com/zenai/ticketonline/MainActivity.java
@@ -2,129 +2,49 @@ package com.zenai.ticketonline;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
+import android.view.MenuItem;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.zenai.ticketonline.models.data_mahasiswa;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.zenai.ticketonline.fragments.WisataFragment;
 
-import java.util.Collections;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    //Deklarasi Variable
-    private ProgressBar progressBar;
-    private EditText NIM, Nama, Jurusan;
-    private FirebaseAuth auth;
-    private Button Logout, Simpan, ShowData;
-
+public class MainActivity extends AppCompatActivity implements  BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        progressBar = findViewById(R.id.progress);
-        progressBar.setVisibility(View.GONE);
-
-        //Inisialisasi ID (Button)
-
-        Logout = findViewById(R.id.logout);
-        Logout.setOnClickListener(this);
-        Simpan = findViewById(R.id.save);
-        Simpan.setOnClickListener(this);
-        ShowData = findViewById(R.id.showdata);
-        ShowData.setOnClickListener(this);
-
-        auth = FirebaseAuth.getInstance(); //Mendapakan Instance Firebase Autentifikasi
-
-        //Inisialisasi ID (EditText)
-        NIM = findViewById(R.id.nim);
-        Nama = findViewById(R.id.nama);
-        Jurusan = findViewById(R.id.jurusan);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        loadFragment(new WisataFragment());
 
     }
 
 
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-
-            case R.id.save:
-                //Mendapatkan UserID dari pengguna yang Terautentikasi
-                String getUserID = auth.getCurrentUser().getUid();
-
-                //Mendapatkan Instance dari Database
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference getReference;
-
-                //Menyimpan Data yang diinputkan User kedalam Variable
-                String getNIM = NIM.getText().toString();
-                String getNama = Nama.getText().toString();
-                String getJurusan = Jurusan.getText().toString();
-
-                getReference = database.getReference(); // Mendapatkan Referensi dari Database
-
-                // Mengecek apakah ada data yang kosong
-                if (isEmpty(getNIM) || isEmpty(getNama) || isEmpty(getJurusan)) {
-                    //Jika Ada, maka akan menampilkan pesan singkan seperti berikut ini.
-                    Toast.makeText(MainActivity.this, "Data tidak boleh ada yang kosong", Toast.LENGTH_SHORT).show();
-                } else {
-                    /*
-                    Jika Tidak, maka data dapat diproses dan meyimpannya pada Database
-                    Menyimpan data referensi pada Database berdasarkan User ID dari masing-masing Akun
-                    */
-                    getReference.child("Admin").child(getUserID).child("Mahasiswa").push()
-                            .setValue(new data_mahasiswa(getNIM, getNama, getJurusan))
-                            .addOnSuccessListener(this, new OnSuccessListener() {
-                                @Override
-                                public void onSuccess(Object o) {
-                                    //Peristiwa ini terjadi saat user berhasil menyimpan datanya kedalam Database
-                                    NIM.setText("");
-                                    Nama.setText("");
-                                    Jurusan.setText("");
-                                    Toast.makeText(MainActivity.this, "Data Tersimpan", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                }
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.action_add_tour:
+                fragment = new WisataFragment();
                 break;
-
-            case R.id.showdata:
-                startActivity(new Intent(MainActivity.this, MyListData.class));
-                break;
-
-            case R.id.logout:
-                // Statement program untuk logout/keluar
-                AuthUI.getInstance()
-                        .signOut(this)
-                        .addOnCompleteListener(new OnCompleteListener() {
-                            @Override
-                            public void onComplete(@NonNull Task task) {
-                                Toast.makeText(MainActivity.this, "Logout Berhasil", Toast.LENGTH_SHORT).show();
-                                finish();
-                            }
-                        });
-                break;
+            default:fragment=null;
         }
+        return loadFragment(fragment);
 
-    }
-
-    // Mengecek apakah ada data yang kosong
-    private boolean isEmpty(String s){
-        return TextUtils.isEmpty(s);
     }
 
 
---
+++ app/src/main/java/com/zenai/ticketonline/fragments/WisataFragment.java
@@ -0,0 +1,84 @@
+package com.zenai.ticketonline.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.zenai.ticketonline.R;
+
+
+public class WisataFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public WisataFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_wisata, container, false);
+        final EditText name = view.findViewById(R.id.nameText);
+        final EditText location = view.findViewById(R.id.locationText);
+        final EditText price = view.findViewById(R.id.priceText);
+        final EditText description = view.findViewById(R.id.descriptionText);
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
